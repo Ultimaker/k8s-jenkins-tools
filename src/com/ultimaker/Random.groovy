8842c2d05e1793825db5f6e28af338d4bacc2adc
Yordi Pauptit <hi@yordipauptit.com>
2018-02-15 08:41:07 +0100

    should grab the apache commons-lang first

--- src/com/ultimaker/Random.groovy
+++ src/com/ultimaker/Random.groovy
@@ -1,6 +1,9 @@
 #!/usr/bin/groovy
 package com.ultimaker
 
+@Grab('commons-lang:commons-lang:2.6')
+import org.apache.commons.lang.RandomStringUtils
+
 def string(int length) {
   String charset = (('a'..'z') + ('A'..'Z') + ('0'..'9')).join()
 
