1f5b1f00ae29bf0e439a156554e40c62687322fd
Rob Bast <rob.bast@gmail.com>
2019-08-07 10:25:18 +0200

    add Slack helper methods

---
+++ src/com/ultimaker/Slack.groovy
@@ -0,0 +1,42 @@
+#!/usr/bin/groovy
+
+package com.ultimaker
+
+def playgroundCreated(
+  String channel,
+  String name,
+  String buildUrl,
+  String frontendBranch,
+  String integrationServiceBranch,
+  String legacyBranch,
+  String expiresIn,
+  String clusterDomain
+) {
+  def attachments = [[
+    color: "good",
+    fallback: "Playground \"${name}\" was created.",
+    pretext: "Playground \"<https://playground-${name}.ultimaker.${clusterDomain}|${name}>\" was created ( <${env.BUILD_URL}|job> / <${env.BUILD_URL}console|console> ).",
+    fields: [
+      [
+        title: "Endpoints",
+        value: """
+          - https://playground-${name}.ultimaker.${clusterDomain}
+          - https://playground-${name}.cms.${clusterDomain}/admin
+          - https://playground-${name}.integration-service.${clusterDomain}
+          """.stripIndent(),
+        short: false
+      ],
+      [ title: "Frontend branch", value: "<https://github.com/Ultimaker/Ultimaker.com-frontend/tree/${frontendBranch}|${frontendBranch}>", short: true ],
+      [ title: "Legacy branch", value: "<https://github.com/Ultimaker/Ultimaker.com/tree/${legacyBranch}|${legacyBranch}>", short: true ],
+      [ title: "Integration-Service branch", value: "<https://github.com/Ultimaker/Ultimaker.com-integration-service/tree/${integrationServiceBranch}|${integrationServiceBranch}>", short: true ],
+      [ title: "Expires in", value: expiresIn, short: true ],
+    ]
+  ]]
+
+  slackSend channel: channel, attachments: attachments
+}
+
+def playgroundFailed(String channel, String name, String buildUrl) {
+  slackSend color: 'danger', channel: '#ci-playgrounds',
+        message: "Creating playground \"${name}\" failed ( <${buildUrl}|job> / <${buildUrl}console|console> )."
+}
