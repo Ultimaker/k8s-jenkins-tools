4bcaa4b2d3331507952b42a09ce8920fcfa97ca7
Rob Bast <rob.bast@gmail.com>
2019-08-08 09:38:55 +0200

    poor man approach to make sure we stay within limit

--- vars/generateLabel.groovy
+++ vars/generateLabel.groovy
@@ -2,6 +2,11 @@
 
 GString call(String prefix) {
 
+  // max length is 64 characters, UUID is 36, leaves us with 28
+  if (prefix.length() > 28) {
+    prefix = prefix.take(28)
+  }
+
   return "$prefix-${UUID.randomUUID().toString()}"
 
 }
