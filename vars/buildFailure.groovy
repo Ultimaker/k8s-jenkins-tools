a7c333375a0275b9588e8cc873e72d0505fecb91
Nicolo Giacomello <nicologiacomello@gmail.com>
2019-08-21 11:41:32 +0200

    Fix webhookUrl to ci-builds-connector (#2)

--- vars/buildFailure.groovy
+++ vars/buildFailure.groovy
@@ -24,8 +24,7 @@ def call(
   office365ConnectorSend color: '#d93232',
     message: "Failed building \"${component}\" ( [job](${buildUrl}) / [console](${buildUrl}console) ).",
     status: 'Failure',
-    // ci-playgrounds-connector
-    webhookUrl: 'https://outlook.office.com/webhook/4829f4a1-35aa-4dca-9ad7-89ac07833f0a@aecc8a99-f9b2-4dad-9cf1-038b83a3ff5f/JenkinsCI/4aee1620d03b467eb08e51f1be42e441/cdfdcb89-4370-47bb-95e6-921ab334adb2'
-
+    // ci-builds-connector
+    webhookUrl: 'https://outlook.office.com/webhook/4829f4a1-35aa-4dca-9ad7-89ac07833f0a@aecc8a99-f9b2-4dad-9cf1-038b83a3ff5f/JenkinsCI/f708a35d1e664c1fb5834c370e974533/cdfdcb89-4370-47bb-95e6-921ab334adb2'
 
 }
